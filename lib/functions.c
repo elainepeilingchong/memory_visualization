b1106f4e2d9bb0ca5345ad5c765765ee43790f51
elaine chong <elainepeilingchong@gmail.com>
2019-03-12 02:47:01 +0000

    Added Functions
    Create mask & calculation for bits

--- lib/functions.c
+++ lib/functions.c
@@ -1,1 +1,23 @@
-test1
+#include "functions.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+addr_t create_mask(bit_t bits, mask_type type) {
+    addr_t mask   = 0;
+    addr_t masker = type == LOW_ORDER_MASK? BITMASK_LSB: BITMASK_MSB;
+    for (bit_t i = 0; i < bits; ++i) {
+        mask |= masker;
+        if (type == LOW_ORDER_MASK) {
+            masker <<= 1;
+        } else {
+            masker >>= 1;
+        }
+    }
+    
+    return mask;
+}
+int calculateBitsRequired()
+{
+    int pageSizeBit = (int)(log((double)PAGE_SIZE) / log((double)2));
+    return SYS_BITS - pageSizeBit;
+}
--- lib/functions.h
+++ lib/functions.h
@@ -1,1 +1,27 @@
-test2
+#ifndef functions_h
+#define functions_h
+
+//define constant
+#define BITMASK_LSB 0x1
+#define BITMASK_MSB 0x80
+#define OFFSET_BITS 8
+#define PAGE_SIZE 256
+#define MAX_BYTES 20480
+#define MIN_BYTES 2048
+#define ADDEESSABLE_MEMORY 65536
+#define SYS_BITS 16
+
+typedef enum {
+    LOW_ORDER_MASK,
+    HIGH_ORDER_MASK
+} mask_type;
+typedef unsigned char  bit_t;
+typedef unsigned short addr_t;
+
+addr_t create_mask(bit_t, mask_type);
+
+//declare functions
+int calculateBitsRequired(void);
+
+#endif 
+
